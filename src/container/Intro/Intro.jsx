328acf60a04247331a7d443f9020079eec59d8d0
AlbertoLanchas <albertolsn7@gmail.com>
2022-10-11 16:00:39 +0200

    laurels & gallery
    Signed-off-by: AlbertoLanchas <albertolsn7@gmail.com>

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useState, useRef } from "react";
+import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
 
-import './Intro.css';
+import { meal } from "../../constants";
+import "./Intro.css";
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+  const [playVideo, setPlayVideo] = useState(false);
+  const vidRef = useRef();
+
+  return (
+    <div className="app__video">
+      <video
+        ref={vidRef}
+        src={meal}
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+      />
+      <div
+        className={`flex__center ${
+          playVideo ? "app__video-overlay_play" : "app__video-overlay"
+        }`}
+      >
+        <div
+          className="app__video-overlay_circle flex__center"
+          onClick={() => {
+            setPlayVideo(!playVideo);
+            if (playVideo) {
+              vidRef.current.pause();
+            } else {
+              vidRef.current.play();
+            }
+          }}
+        >
+          {playVideo ? (
+            <BsPauseFill color="#fff" fontSize={30} />
+          ) : (
+            <BsFillPlayFill color="#fff" fontSize={30} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Intro;
